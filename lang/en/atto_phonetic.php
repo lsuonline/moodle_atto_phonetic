09dd7890e58956d1f07d7ffaeff4cc6b977e5c2e
David Lowe <dalotech@gmail.com>
2022-09-12 16:34:44 -0600

    DALO - Initial Commit.

---
+++ lang/en/atto_phonetic.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Phonetic Plugin for the Atto Editor
+ *
+ * @package   atto_phonetic
+ * @copyright 2008 onwards Louisiana State University
+ * @copyright 2008 onwards David Lowe, Robert Russo
+ * @co-author Disha Devaiya
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['librarygroup1'] = 'Pulmonic Consonants';
+$string['librarygroup1_desc'] = 'TeX commands listed on the Pulmonic Consonants tab.';
+$string['librarygroup2'] = 'Vowels';
+$string['librarygroup2_desc'] = 'TeX commands listed on the Vowels tab.';
+$string['librarygroup3'] = 'Non-Pulmonic Consonants';
+$string['librarygroup3_desc'] = 'TeX commands listed on the Non-Pulmonic Consonants tab.';
+$string['librarygroup4'] = 'Other';
+$string['librarygroup4_desc'] = 'TeX commands listed on the Other tab.';
+$string['librarygroup5'] = 'Spacing Modifiers';
+$string['librarygroup5_desc'] = 'TeX commands listed on the Spacing Modifiers tab.';
+$string['librarygroup6'] = 'Combining';
+$string['librarygroup6_desc'] = 'TeX commands listed on the Combining tab.';
+
+$string['pluginname'] = 'Phonetic';
+$string['preview'] = 'Phonetic preview';
+$string['cursorinfo'] = 'An arrow indicates the position that new elements from the element library will be inserted.';
+$string['savephonetic'] = 'Save Phonetic';
+$string['settings'] = 'Phonetic editor settings';
+$string['update'] = 'Update';
+$string['base_symbol'] = 'Base Symbol';
